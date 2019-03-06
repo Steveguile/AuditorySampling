542c5a9e859dafa7b7df43b4b53b435e56246904
Steveguile <stephenewhitehead@outlook.com>
2019-03-06 18:15:25 +0000

    Cross validation

--- DataMiner.py
+++ DataMiner.py
@@ -1,57 +1,100 @@
 import pandas as pd
-from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
-from sklearn import exceptions
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score,make_scorer
+from sklearn.model_selection import GridSearchCV
 import warnings
+from sklearn import exceptions
+from sklearn.svm import SVC
 from tabulate import tabulate
-
-# Credit to https://towardsdatascience.com/the-art-of-effective-visualization-of-multi-dimensional-data-6c7202990c57
+from copy import deepcopy
 
 warnings.filterwarnings(action="ignore", category=exceptions.UndefinedMetricWarning) # If it's 0 that's fine
 
-def get_accuracy_scores(classifier, actual, predicted):
 
-    classification_accuracy = accuracy_score(actual, predicted)
-    precision = precision_score(actual, predicted, pos_label="Yes")
-    recall = recall_score(actual, predicted, pos_label="Yes")
-    fscore = f1_score(actual, predicted, pos_label="Yes")
 
-    print("x" * 100, "\n", classifier, "\nx" + "x" * 99)
-    print(tabulate([["Classification Accuracy", classification_accuracy],
-                    ["Precision", precision],
-                    ["Recall", recall],
-                    ["F-Score", fscore]],
-                   headers=["", "Score"]))
+def perform_cv(dataset, target, classifier, parameters, scorers):
+    gs = GridSearchCV(classifier,
+                      param_grid=parameters,
+                      scoring=scorers, cv=5, refit=False,
+                      return_train_score=False)  # refit='precision' only returns best precision
 
-    return classification_accuracy, precision, recall, fscore
+    gs.fit(dataset, target)
+
+    classifier_name = str(classifier).split('(')[0]
+    params = gs.cv_results_.get('params')
+
+    # Do ranking myself because in-built ones don't show results for every parameter
+    class_acc = list(gs.cv_results_.get('mean_test_Classificiation_Accuracy'))
+    class_pre = list(gs.cv_results_.get('mean_test_Precision'))
+    class_rec = list(gs.cv_results_.get('mean_test_Recall'))
+    class_fscr = list(gs.cv_results_.get('mean_test_f_score'))
+
+    classifier_stats = []
+
+    # TODO: Pretend this doesn't exist
+    if len(params) == 1:
+        print("x" * 100, "\n", classifier_name, '('
+              , str(params).replace('\'', '').replace('[', '').replace('{', '').replace('}', '').replace(']', '').replace(':', ' =') # TODO: Disgusteng!  do regex for this later
+              ,")\nx" + "x" * 99)
+        print(tabulate([["Classification Accuracy", str(class_acc).replace('[', '').replace(']', '')],
+                        ["Precision", str(class_pre).replace('[', '').replace(']', '')],
+                        ["Recall", str(class_rec).replace('[', '').replace(']', '')],
+                        ["F-Score", str(class_fscr).replace('[', '').replace(']', '')]],
+                       headers=["", "Score"]))
+
+
+    else:
+        for i, parameter in enumerate(params):
+            print("x" * 100, "\n", classifier_name, '('
+                  , str(parameter).replace('\'', '').replace('[', '').replace('{', '').replace('}', '').replace(']','').replace(':', ' =')
+                  , ")\nx" + "x" * 99)
+            print(tabulate([["Classification Accuracy", str(class_acc[i])],
+                            ["Precision", str(class_pre[i])],
+                            ["Recall", str(class_rec[i])],
+                            ["F-Score", str(class_fscr[i])]],
+                           headers=["", "Score"]))
+
 
 
 def main():
+
     csv_file = pd.read_csv(r"E:\Programming\Projects\Dissertation\SyntheticDataGenerator\traffic_audio.csv", header=0)
     csv_headers = list(csv_file.columns.values)
     dataset = csv_file.drop("reference", axis=1)
     headers = list(dataset.columns.values)
     attribute_headers = headers[0:len(headers) - 1]
 
-    #Shuffle Dataset
+    # Shuffle Dataset
     dataset = dataset.sample(frac=1, random_state=42).reset_index(drop=True)
 
     target = list(dataset["TrafficIncident"])
     dataset = dataset.drop("TrafficIncident", axis=1).get_values()
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        dataset, target, test_size=0.4, random_state=42)
+    classifiers = {'knn': KNeighborsClassifier(),
+                   'svc': SVC()
+                   }
 
-    classifiers = [
-        SVC(gamma=0.001, C=100.),
-        KNeighborsClassifier(n_neighbors=3)
-    ]
+    parameters = {'knn': {'n_neighbors': [1, 3, 5]
+                          },
+                  'svc': {'gamma': [0.001],
+                          'C': [100.]
+                          }
+                  }
 
-    for classifier in classifiers:
-        classifier.fit(X_train, y_train)
-        y_predict = classifier.predict(X_test)
-        get_accuracy_scores(classifier, y_test, y_predict)
+    # Make own SCORERS as need different pos_label
+    class_accuracy = make_scorer(accuracy_score)
+    precision = make_scorer(precision_score, pos_label='Yes')
+    recall = make_scorer(recall_score, pos_label='Yes')
+    f_score = make_scorer(f1_score, pos_label='Yes')
+    scorers = {'Classificiation_Accuracy': class_accuracy,
+               'Precision': precision,
+               'Recall': recall,
+               'f_score': f_score
+               }
+
+    for c_key, classifier in classifiers.items():
+        for p_key, c_params in parameters.items():
+            if p_key == c_key:
+                perform_cv(dataset, target, classifier, c_params, scorers)
 
 main()
